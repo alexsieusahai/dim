fdd6c5c296f2c2bc02cc0d5780542632f0451db3
ItsPax <alexsieu14@gmail.com>
2018-03-28 01:09:10 -0600

    cleaning repo

--- editor.py
+++
@@ -1,9 +0,0 @@
-
-class Editor:
-    def __init__(self,cursesscr,currentLine):
-        self.currentLine = self.topLine = currentLine
-        self.currentLineCount = 0
-        self.currentLineIndex = 0
-        self.scr = cursesscr
-        self.lineLinkedList = None
-
