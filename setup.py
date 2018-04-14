918ca0714ee54e9aeb0d3bc7f167720dd8230dc3
ItsPax <alexsieu14@gmail.com>
2018-04-14 13:35:27 -0600

    i think alias is working now

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-# have to check if environ is windows, and if it is handle accordingly
+# what does vim do?
 
 import os, sys
 
 shell_name = os.environ["SHELL"].split('/')[-1]
 dim_dir = os.getcwd()
 os.chdir(os.path.expanduser('~'))
+
 with open('.'+shell_name+'rc', 'a') as fs:
     fs.write('\n'+
             "# this is the alias for dim\n"+
-            'alias="python3 '+dim_dir+'/main.py"\n'
+            'alias dim="cd '+dim_dir+'/src; python3 main.py"\n'
             )
